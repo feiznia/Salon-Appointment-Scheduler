87a818bfadb3660aad8c64e5da29e37324dc917b
Diba <dibasixx@gmail.com>
2024-05-11 01:02:11 +0330

    first commit

---
+++ salon.sql
@@ -0,0 +1,19 @@
+create database salon;
+create table customers (
+    customer_id serial primary key,
+    phone varchar(20) unique,
+    name text,
+);
+create table appointments (
+    appointment_id serial primary key,
+    customer_id integer not null,
+    service_id integer not null,
+    time varchar,
+);
+create table services (
+    service_id serial primary key,
+    name text
+);
+alter table appointments add foreign key(customer_id) references customers(customer_id);
+alter table appointments add foreign key(service_id) references services(service_id);
+insert into services(name) values('manicure', 'pedicure', 'acrylics')
